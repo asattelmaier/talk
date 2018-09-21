2391563f9f13b2075ff5c17e2aacc69265a95747
Alexander Sattelmaier <alexander.sattelmaier@gmail.com>
2018-09-21 11:32:49 +0200

    implemented socket communication between sender and receiver

--- src/Receiver.java
+++ src/Receiver.java
@@ -1,2 +1,37 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Receiver {
+    public static void main(String args[]) {
+        ServerSocket echoServer = null;
+        String line;
+        DataInputStream is;
+        PrintStream os;
+        Socket clientSocket;
+
+        try {
+            echoServer = new ServerSocket(9999);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        try {
+            if (echoServer != null) {
+                clientSocket = echoServer.accept();
+                is = new DataInputStream(clientSocket.getInputStream());
+                os = new PrintStream(clientSocket.getOutputStream());
+
+                while (true) {
+                    line = is.readLine();
+                    System.out.println(line);
+                    os.println("received");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 }
--- src/Sender.java
+++ src/Sender.java
@@ -1,2 +1,68 @@
-public class Sender {
+import java.net.*;
+import java.io.*;
+
+public class Sender extends Thread {
+    private Socket client = null;
+    private DataInputStream is = null;
+    private DataOutputStream os = null;
+    private Boolean connected = false;
+
+    private Sender(String host, int portNumber) {
+        try {
+            client = new Socket(host, portNumber);
+            is = new DataInputStream(client.getInputStream());
+            os = new DataOutputStream(client.getOutputStream());
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host: hostname");
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to: hostname");
+        }
+
+        connected = client != null && os != null && is != null;
+    }
+
+    private void send(String[] args) {
+        try {
+            for (String s: args) {
+                os.writeBytes(s + "\n");
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Trying to connect to unknown host: " + e);
+        } catch (IOException e) {
+            System.err.println("IOException:  " + e);
+        }
+    }
+
+    private void receiveData() {
+        try {
+            String responseLine;
+            while ((responseLine = is.readLine()) != null) {
+                System.out.println("Server: " + responseLine);
+                if (responseLine.contains("Ok")) {
+                    break;
+                }
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Trying to connect to unknown host: " + e);
+        } catch (IOException e) {
+            System.err.println("IOException:  " + e);
+        }
+    }
+
+    private void closeConnection()throws IOException {
+        is.close();
+        os.close();
+        client.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Sender sender = new Sender("localhost", 9999);
+
+        if (sender.connected) {
+            System.err.println("Connection established.");
+            sender.send(args);
+            sender.receiveData();
+            sender.closeConnection();
+        }
+    }
 }
