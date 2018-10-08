d968f9e4209b6cd29b0ed396ff889a448871840f
FourBitsOneNibble <fourbitsonenibble@gmail.com>
2018-10-08 14:13:53 +0200

    Fix crash with less than three arguments
    Also changed the order of provided arguments to remote, talkPort and listenPort.

--- src/main/java/com/app/talk/Talk.java
+++ src/main/java/com/app/talk/Talk.java
@@ -23,14 +23,18 @@ public class Talk {
     /**
      * A simple talk/chat application.
      * @param args - arguments transferred from the operating system
-     *             args[0]: the port to listen to (default: 2048)
-     *             args[1]: the port to talk to (default: 2049)
-     *             args[2]: remote machine to talk to (default: localhost)
+     *             args[0]: remote machine to talk to (default: localhost)
+     *             args[1]: the port to talk to (default: 2049)            
+     *             args[2]: the port to listen to (default: 2048)
      */
     public static void main(String[] args) {
-        int listenPort = args[0].length() > 0 ? Integer.parseInt(args[0]) : 2048;
-        int talkPort = args[1].length() > 0 ? Integer.parseInt(args[1]) : 2049;
-        String remote = args[2].length() > 0 ? args[2] : "localhost";
+    	String remoteArg = args.length > 0 ? args[0] : "";
+    	String talkPortArg = args.length > 1 ? args[1] : "";
+    	String listenPortArg = args.length > 2 ? args[2] : "";
+
+    	String remote = remoteArg.length() > 0 ? remoteArg : "localhost";
+        int talkPort = talkPortArg.length() > 0 ? Integer.parseInt(talkPortArg) : 2049;
+        int listenPort = listenPortArg.length() > 0 ? Integer.parseInt(listenPortArg) : 2048;
 
         Talk talk = new Talk();
 
