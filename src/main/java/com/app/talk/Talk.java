6a9346a9ab61ef2d257da2822df52ec5c1887381
Alexander Sattelmaier <alexander.sattelmaier@gmail.com>
2018-11-04 16:18:18 +0100

    implemented validation for arguments

--- src/main/java/com/app/talk/Talk.java
+++ src/main/java/com/app/talk/Talk.java
@@ -9,6 +9,35 @@ public class Talk {
     private static final int DEFAULT_TALK_PORT = 2049;
     private static final int DEFAULT_LISTEN_PORT = 2048;
     private static final String DEFAULT_IP = "localhost";
+    private static int listenPort;
+    private static int talkPort;
+    private static String remote;
+
+    private static void validateArgs(String[] args) {
+        int argLength = args.length;
+
+        if (argLength <= 3) {
+            try {
+                if (argLength > 0) {
+                    Integer.parseInt(args[0]);
+                }
+                if (argLength > 1) {
+                    Integer.parseInt(args[1]);
+                }
+            } catch (NumberFormatException e) {
+                System.err.println("Given Port is not valid!");
+            }
+        } else {
+            System.err.println("To many arguments given. Type 'help' for further Information.");
+            System.exit(-1);
+        }
+    }
+
+    private static void setPortsAndRemote(String[] args) {
+        listenPort = args.length > 0 ? Integer.parseInt(args[0]) : DEFAULT_LISTEN_PORT;
+        talkPort = args.length > 1 ? Integer.parseInt(args[1]) : DEFAULT_TALK_PORT;
+        remote = args.length > 2 ? args[2] : DEFAULT_IP;
+    }
 
     private static String getUserNameFromInput() {
         Scanner scanner = new Scanner(System.in);
@@ -27,9 +56,8 @@ public class Talk {
      *             args[2]: remote machine to talk to (default: localhost)
      */
     public static void main(String[] args) {
-        int listenPort = args.length > 0 ? Integer.parseInt(args[0]) : DEFAULT_LISTEN_PORT;
-        int talkPort = args.length > 1 ? Integer.parseInt(args[1]) : DEFAULT_TALK_PORT;
-        String remote = args.length > 2 ? args[2] : DEFAULT_IP;
+        validateArgs(args);
+        setPortsAndRemote(args);
 
         String userName = getUserNameFromInput();
 
