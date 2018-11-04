69e0e4eaa389557176ff6f5f21525cc2ea939fbf
Alexander Sattelmaier <alexander.sattelmaier@gmail.com>
2018-11-04 15:47:11 +0100

    refactored talk main method

--- src/main/java/com/app/talk/Talk.java
+++ src/main/java/com/app/talk/Talk.java
@@ -9,33 +9,29 @@ public class Talk {
     private static final int DEFAULT_TALK_PORT = 2049;
     private static final int DEFAULT_LISTEN_PORT = 2048;
     private static final String DEFAULT_IP = "localhost";
-    private Scanner scanner = new Scanner(System.in);
 
-    private String getUserNameFromInput() {
-        return this.scanner.nextLine();
+    private static String getUserNameFromInput() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your username: ");
+
+        return scanner.nextLine();
     }
 
     /**
      * A simple talk/chat application.
      *
      * @param args - arguments transferred from the operating system
-     *             args[0]: remote machine to talk to (default: localhost)
+     *             args[0]: the port to listen to (default: 2048)
      *             args[1]: the port to talk to (default: 2049)
-     *             args[2]: the port to listen to (default: 2048)
+     *             args[2]: remote machine to talk to (default: localhost)
      */
     public static void main(String[] args) {
-        String remoteArg = args.length > 0 ? args[0] : "";
-        String talkPortArg = args.length > 1 ? args[1] : "";
-        String listenPortArg = args.length > 2 ? args[2] : "";
+        int listenPort = args.length > 0 ? Integer.parseInt(args[0]) : DEFAULT_LISTEN_PORT;
+        int talkPort = args.length > 1 ? Integer.parseInt(args[1]) : DEFAULT_TALK_PORT;
+        String remote = args.length > 2 ? args[2] : DEFAULT_IP;
 
-        String remote = remoteArg.length() > 0 ? remoteArg : DEFAULT_IP;
-        int talkPort = talkPortArg.length() > 0 ? Integer.parseInt(talkPortArg) : DEFAULT_TALK_PORT;
-        int listenPort = listenPortArg.length() > 0 ? Integer.parseInt(listenPortArg) : DEFAULT_LISTEN_PORT;
-
-        Talk talk = new Talk();
-
-        System.out.print("Enter your username: ");
-        String userName = talk.getUserNameFromInput();
+        String userName = getUserNameFromInput();
 
         Receiver receiver = new Receiver(listenPort);
         Sender sender = new Sender(remote, talkPort, userName);
