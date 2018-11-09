a14cd534d708078a3d735f10ec2764ae5842c25f
michoff <michael.hoffmann@stud.hs-emden-leer.de>
2018-11-09 17:04:48 +0100

    Added doc comments for var / methods of Sender

--- src/main/java/com/app/talk/Sender.java
+++ src/main/java/com/app/talk/Sender.java
@@ -11,11 +11,30 @@ import java.util.concurrent.TimeUnit;
  */
 public class Sender implements Runnable {
 
+	/**
+	 * The ip-address of the other Host.
+	 */
     private String remoteHost;
+    /**
+     * The port which the addressed host listens to.
+     */
     private int port;
+    /**
+     * A dummy Socket that represents the receiving Socket of the other Host. 
+     * Contains the ip-adress and listening port of the other Host.
+     */
     private Socket client = null;
+    /**
+     * A DataOutputStream containing the OutputStream of the client Socket.
+     */
     private DataOutputStream outputStream = null;
+    /**
+     * The username of the sending host.
+     */
     private String userName;
+    /**
+     * A scanner to receive User keyboard input
+     */
     private Scanner scanner = new Scanner(System.in);
 
     /**
@@ -30,7 +49,11 @@ public class Sender implements Runnable {
         this.userName = userName;
     }
 
-
+    /**
+     * The the executing method of the class.
+     * This method is being called by the start()-method of an Thread object containing 
+     * a Sender object to establish the outgoing connection to the other host.
+     */
     public void run() {
         System.out.println("Waiting for connection to: " + this.remoteHost + ":" + this.port + "...");
         this.establishConnection();
@@ -45,11 +68,15 @@ public class Sender implements Runnable {
         this.closeConnection();
         System.out.println("Connection closed.");
     }
-
+    /**
+     * A method to send the username of the sending Host.
+     */
     private void sendUserName() {
         this.sendMsg("?userName&" + this.userName);
     }
-
+    /**
+     * A method to establish the connection to another host over the network.
+     */
     private void establishConnection() {
         try {
             this.connect();
@@ -57,11 +84,17 @@ public class Sender implements Runnable {
             this.reconnect();
         }
     }
-
+    /**
+     * A method that creates a dummy Socket as a receiving end for outgoing communication.
+     * 
+     * @throws IOException
+     */
     private void connect() throws IOException {
         this.client = new Socket(this.remoteHost, this.port);
     }
-
+    /**
+     * A method that tries to reconnect after 10 seconds
+     */
     private void reconnect() {
         try {
             TimeUnit.SECONDS.sleep(10);
@@ -70,7 +103,9 @@ public class Sender implements Runnable {
             System.err.println(e.getMessage());
         }
     }
-
+    /**
+     * Initializes the outputStream of the Sender object
+     */
     private void setOutputStream() {
         try {
             this.outputStream = new DataOutputStream(client.getOutputStream());
@@ -78,7 +113,10 @@ public class Sender implements Runnable {
             System.err.println("Couldn't get I/O for the connection to: " + this.remoteHost);
         }
     }
-
+    /**
+     * A method that creates a loop in which the user keyboard input is being taken in and
+     * sent to the other Host.
+     */
     private void sendUserInput() {
         String userInput;
 
@@ -92,7 +130,10 @@ public class Sender implements Runnable {
             }
         }
     }
-
+    /**
+     * A method that receives a String message and writes it in sequences of bytes to the other host.
+     * @param msg - message that should be sent to the other host.
+     */
     private void sendMsg(String msg) {
         try {
             this.outputStream.writeBytes(msg + "\n");
@@ -101,7 +142,9 @@ public class Sender implements Runnable {
             System.err.println("IOException: " + e);
         }
     }
-
+    /**
+     * Closes the client Socket as well as its OutputStream.
+     */
     private void closeConnection() {
         try {
         	this.client.close(); //closes OutputStream as well
