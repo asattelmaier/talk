0072b93bcfd8efea665837879c178c16a5582f19
Alexander Sattelmaier <alexander.sattelmaier@gmail.com>
2018-11-17 08:51:16 +0100

    implemented sender connect unit test

--- src/main/java/com/app/talk/Sender.java
+++ src/main/java/com/app/talk/Sender.java
@@ -11,16 +11,16 @@ import java.util.concurrent.TimeUnit;
  */
 public class Sender implements Runnable {
 
-	/**
-	 * The ip-address of the other Host.
-	 */
+    /**
+     * The ip-address of the other Host.
+     */
     private String remoteHost;
     /**
      * The port which the addressed host listens to.
      */
     private int port;
     /**
-     * A dummy Socket that represents the receiving Socket of the other Host. 
+     * A dummy Socket that represents the receiving Socket of the other Host.
      * Contains the ip-address and listening port of the other Host.
      */
     private Socket client = null;
@@ -56,18 +56,22 @@ public class Sender implements Runnable {
      * a Sender object to establish the outgoing connection to the other host.
      */
     public void run() {
-        System.out.println("Waiting for connection to: " + this.remoteHost + ":" + this.port + "...");
-        this.establishConnection();
-        System.out.println("Connection established.");
+        try {
+            System.out.println("Waiting for connection to: " + this.remoteHost + ":" + this.port + "...");
+            this.establishConnection();
+            System.out.println("Connection established.");
 
-        this.setOutputStream();
+            this.setOutputStream();
 
-        this.sendUserName();
+            this.sendUserName();
 
-        this.sendUserInput();
+            this.sendUserInput();
 
-        this.closeConnection();
-        System.out.println("Connection closed.");
+            this.closeConnection();
+            System.out.println("Connection closed.");
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
     /**
@@ -80,27 +84,27 @@ public class Sender implements Runnable {
     /**
      * A method to establish the connection to another host over the network.
      */
-    private void establishConnection() {
+    private void establishConnection() throws IOException {
         try {
             this.connect();
-        } catch (IOException e) {
+        } catch (ConnectException e) {
             this.reconnect();
         }
     }
 
     /**
      * A method that creates a dummy Socket as a receiving end for outgoing communication.
-     * 
+     *
      * @throws IOException IOExceptions
      */
-    private void connect() throws IOException {
+    void connect() throws IOException {
         this.client = new Socket(this.remoteHost, this.port);
     }
 
     /**
      * A method that tries to reconnect after 10 seconds
      */
-    private void reconnect() {
+    private void reconnect() throws IOException {
         try {
             TimeUnit.SECONDS.sleep(10);
             this.establishConnection();
--- src/test/java/com/app/talk/SenderTest.java
+++ src/test/java/com/app/talk/SenderTest.java
@@ -1,22 +1,26 @@
 package com.app.talk;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.net.ConnectException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 import static org.mockito.Matchers.endsWith;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 class SenderTest {
+    private final String USERNAME = "Frank Elstner";
+    private final int PORT = 2049;
+    private final String REMOTE_HOST = "localhost";
 
     @Test
     @DisplayName("Waiting for Connection")
     void startSender() {
-        String USERNAME = "Frank Elstner";
-        int PORT = 2049;
-        String REMOTE_HOST = "localhost";
-
         PrintStream out = mock(PrintStream.class);
         System.setOut(out);
 
@@ -25,5 +29,13 @@ class SenderTest {
         senderThread.start();
 
         verify(out).println(endsWith(REMOTE_HOST + ":" + PORT + "..."));
+    }
+
+    @Test
+    @DisplayName("Connection Failed")
+    void reconnect() throws IOException {
+        Sender sender = new Sender(REMOTE_HOST, PORT, USERNAME);
+
+        Assertions.assertThrows(ConnectException.class, sender::connect);
     }
 }
